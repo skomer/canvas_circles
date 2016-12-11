b9b3e24146057b821411f3fcbbc57d2d02705b38
Josephine Elder <parusmontanus@gmail.com>
2016-12-11 23:12:50 +0000

    Circles render in delayed animation. Added a wee bit of styling

--- public/app.js
+++ public/app.js
@@ -5,26 +5,34 @@ var app = function() {
 
     var circles = function(event) {
         console.log("circles");
-        ctx.beginPath();
 
-        var radius = 100;
+        var x = 200;
+        var y = 200;
+        var radius = 200;
         var startAngle = 0;
         var endAngle = Math.PI*2;
 
-        for (var i = 0; i < 6; i++) {
-            ctx.arc(200,200, radius, startAngle, endAngle);
-            ctx.moveTo(300-((i+1)*(20)), 200);
-            radius -= 20;
-        }
-        ctx.closePath();
-        ctx.stroke();
+        (function timedCircles(i) {
+            setTimeout(function() {
+                ctx.beginPath();
+                ctx.arc(x, y, radius, startAngle, endAngle);
+                console.log("i:", i);
+                radius -= 10;
+                ctx.moveTo(300-((i+1)*(10)), x);
+                console.log("radius:", radius);
+                if (--i) {
+                    timedCircles(i);
+                }
+            }, 40);
+            ctx.closePath();
+            ctx.stroke();
+        })(21);
     }
 
     var draw = function() {
         ctx.clearRect(0,0,canvas.width, canvas.height);
         circles.draw();
     }
-
 
     canvas.onclick = circles;
 
